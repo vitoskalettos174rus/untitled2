09b3c3eb594c4f895c1c0d92374c3f82afeca656
Vitoskaletos <usolsev87@mail.ru>
2024-02-25 12:12:18 +0500

    Addd

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticket = 5000;
+        int bonus = 20;
+        int bonusMile = ticket/ bonus;
+
+        System.out.println("При покупке билета стоимостью " + ticket + " руб., Ваш бонус составит " + bonusMile + " бонусных миль.");
+    }
+}
